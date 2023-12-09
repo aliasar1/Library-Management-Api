184636bbc46f2dff3fd04aecbe0067344476083c
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2023-12-09 21:59:03 +0500

    Project INIT

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 5050;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
